638e1494ef988f341347823211a5726d3c6ac53a
Julien CHARPENTIER <julien.charpentier@telecom-paristech.fr>
2019-07-19 18:29:22 +0200

    [chore] Improve 401 HTTP response with application/problem+json format

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -37,7 +37,16 @@ class Authenticate
     public function handle(Request $request, Closure $next, $guard = null)
     {
         if ($this->auth->guard($guard)->guest() && ! app()->environment('local')) {
-            return response('Unauthorized.', 401)->header('Access-Control-Allow-Origin', '*');
+            return response(json_encode([
+                'status' => 401,
+                'title' => 'Unauthorized',
+                'detail' => 'This ressource require Authentication. Ask for token first at /oauth/token',
+                'type' => 'https://example.net/authentication-error',
+                'instance' => null,
+                'invalid-params' => null
+            ]), 401)
+                ->header('Access-Control-Allow-Origin', '*')
+                ->header('Content-Type',  'application/problem+json');
         }
 
         return $next($request);
